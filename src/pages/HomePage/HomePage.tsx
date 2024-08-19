9eb78c0debbd0257aa2c01858c7ba4c0efbfc1b9
Harsha Vardhan <javvajiharshavardhan.24@protonmail.com>
2024-08-19 23:25:46 +0530

    updated the grid layout

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Header } from "../../components/Header/Header";
+import { AvatarFallback } from "@/components/ui/avatar";
 
 import netflix_logo from "../../assets/trusted_by/netflix_logo.png";
 import google_logo from "../../assets/trusted_by/google_logo.png";
@@ -7,6 +8,7 @@ import youtube_logo from "../../assets/trusted_by/youtube_logo.png";
 import prime_logo from "../../assets/trusted_by/prime_logo.png";
 import play_logo from "../../assets/trusted_by/play_logo.png";
 import ReactPlayer from "react-player";
+import { Avatar } from "@radix-ui/react-avatar";
 export const HomePage = () => {
   const IMAGES_ARR = [
     { name: "netflix logo", link: netflix_logo },
@@ -68,31 +70,145 @@ export const HomePage = () => {
         <h1 className="font-semibold mb-4">
           Listen to what our users say about us
         </h1>
-        <div className="grid grid-cols-4 grid-rows-4 gap-4">
-          <div className="col-span-1 row-span-1 bg-gray-200 p-4 rounded-lg">
-            Item One
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-rows-2 gap-6 ">
+          {/* Column One */}
+          <div className="col-span-1 flex flex-wrap md:flex-col gap-10">
+            <div className="h-64 bg-white rounded-lg shadow-lg p-4">
+              <p>“Love this new open-source app ...”</p>
+              <div className="flex items-center mt-4">
+                <img
+                  src="jonni-lundy.jpg"
+                  alt="Jonni Lundy"
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="ml-2">
+                  <p className="font-semibold">Steven Tey</p>
+                  <p className="text-sm text-gray-500">@steventey</p>
+                </div>
+              </div>
+            </div>
+            <div className="h-64 bg-white rounded-lg shadow-lg p-4">
+              <p>“Love this new open-source app ...”</p>
+              <div className="flex items-center mt-4">
+                <img
+                  src="jonni-lundy.jpg"
+                  alt="Jonni Lundy"
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="ml-2">
+                  <p className="font-semibold">Steven Tey</p>
+                  <p className="text-sm text-gray-500">@steventey</p>
+                </div>
+              </div>
+            </div>
+            <div className="h-64 bg-white rounded-lg shadow-lg p-4">
+              <p>“Love this new open-source app ...”</p>
+              <div className="flex items-center mt-4">
+                <img
+                  src="jonni-lundy.jpg"
+                  alt="Jonni Lundy"
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="ml-2">
+                  <p className="font-semibold">Steven Tey</p>
+                  <p className="text-sm text-gray-500">@steventey</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-span-2 row-span-2 bg-gray-200 p-4 rounded-lg">
-            Item Two
+          {/* Column Two */}
+          <div className="col-span-1 lg:col-span-2 flex flex-wrap md:flex-col gap-10">
+            <div className="h-80 bg-white rounded-lg shadow-lg p-4">
+              <p>“Loving it so far! Cleaned up ...”</p>
+              <div className="flex items-center mt-4">
+                <img
+                  src="jonni-lundy.jpg"
+                  alt="Jonni Lundy"
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="ml-2">
+                  <p className="font-semibold">Jonni Lundy</p>
+                  <p className="text-sm text-gray-500">@jonnilundy</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-6">
+              <div className="h-96 w-80 col-span-1 row-span-2 bg-white rounded-lg shadow-lg p-4">
+                <p>“Loving it so far! Cleaned up ...”</p>
+                <div className="flex items-center mt-4">
+                  <img
+                    src="jonni-lundy.jpg"
+                    alt="Jonni Lundy"
+                    className="w-8 h-8 rounded-full"
+                  />
+                  <div className="ml-2">
+                    <p className="font-semibold">Jonni Lundy</p>
+                    <p className="text-sm text-gray-500">@jonnilundy</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-10">
+                <div className="h-52 w-80 col-span-1 row-span-2 bg-white rounded-lg shadow-lg p-4">
+                  <p>“Loving it so far! Cleaned up ...”</p>
+                  <div className="flex items-center mt-4">
+                    <img
+                      src="jonni-lundy.jpg"
+                      alt="Jonni Lundy"
+                      className="w-8 h-8 rounded-full"
+                    />
+                    <div className="ml-2">
+                      <p className="font-semibold">Jonni Lundy</p>
+                      <p className="text-sm text-gray-500">@jonnilundy</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="h-52 w-80 col-span-1 row-span-2 bg-white rounded-lg shadow-lg p-4">
+                  <p>“Loving it so far! Cleaned up ...”</p>
+                  <div className="flex items-center mt-4">
+                    <img
+                      src="jonni-lundy.jpg"
+                      alt="Jonni Lundy"
+                      className="w-8 h-8 rounded-full"
+                    />
+                    <div className="ml-2">
+                      <p className="font-semibold">Jonni Lundy</p>
+                      <p className="text-sm text-gray-500">@jonnilundy</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-span-1 row-span-2 bg-gray-200 p-4 rounded-lg">
-            Item Three
-          </div>
-          <div className="col-span-1 row-span-1 bg-gray-200 p-4 rounded-lg">
-            Item Four
-          </div>
-          <div className="col-span-1 row-span-1 bg-gray-200 p-4 rounded-lg">
-            Item Five
-          </div>
-          <div className="col-span-1 row-span-2 bg-gray-200 p-4 rounded-lg">
-            Item Six
-          </div>
-          <div className="col-span-1 row-span-1 bg-gray-200 p-4 rounded-lg">
-            Item Seven
-          </div>
-
-          <div className="col-span-1 row-span-2 bg-gray-200 p-4 rounded-lg">
-            Item Eight
+          {/* Column Three */}
+          <div className="col-span-1 flex flex-wrap md:flex-col gap-10">
+            <div className="h-96 bg-white rounded-lg shadow-lg p-4">
+              <p>“Wow. Onboarded and started ...”</p>
+              <div className="flex items-center mt-4">
+                <img
+                  src="yoni.jpg"
+                  alt="Yoni"
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="ml-2">
+                  <p className="font-semibold">Yoni</p>
+                  <p className="text-sm text-gray-500">@yoni</p>
+                </div>
+              </div>
+            </div>
+            <div className="h-96 bg-white rounded-lg shadow-lg p-4">
+              <p>“Wow. Onboarded and started ...”</p>
+              <div className="flex items-center mt-4">
+                <img
+                  src="yoni.jpg"
+                  alt="Yoni"
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="ml-2">
+                  <p className="font-semibold">Yoni</p>
+                  <p className="text-sm text-gray-500">@yoni</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
